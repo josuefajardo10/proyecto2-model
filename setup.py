fe454ac97230522d7d7ceca6f35b4a7bcfd0c0f5
Josue Fajardo <23001743@galileo.edu>
2024-12-01 19:17:20 -0600

    carga de datos

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Proyecto 2 del curso Product development',
+    author='Universidad Galileo',
+    license='',
+)
